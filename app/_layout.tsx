b6a78ffaa53def76990fce5c5a39db5264722bc4
Jhonatan Ferreira <jhonatan.alanf@gmail.com>
2025-08-04 19:30:24 -0300

    implement sqlite

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,15 @@
 import { Stack } from "expo-router";
+import { SQLiteProvider } from "expo-sqlite";
+import { StatusBar } from "expo-status-bar";
+import { createSchemaIfNeeded } from "../db/setup";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <SQLiteProvider databaseName="todo.db" onInit={createSchemaIfNeeded}>
+        <Stack />
+      </SQLiteProvider>
+      <StatusBar style="auto" />
+    </>
+  );
 }
